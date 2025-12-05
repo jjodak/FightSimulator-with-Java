f83e7daf0c8afa49012ee8d8847bd360c91b0bc6
jjodak <cjh040602@icloud.com>
2025-12-05 22:19:33 +0900

    .

---
+++ src/FightSimulator/Skillset.java
@@ -0,0 +1,12 @@
+package FightSimulator;
+
+public interface Skillset {
+   void skill1(FighterBase target);
+
+   void skill2(FighterBase target);
+
+   void defenseskill();
+
+   void heal();
+
+}
